8d77e7f7670ea1763fa9b33d09c8598aa327b439
swapna1029 <53587556+swapna1029@users.noreply.github.com>
2019-08-09 20:37:17 +0530

    Create count.py

---
+++ count.py
@@ -0,0 +1,12 @@
+def count(a):
+  Q = [500, 200, 100, 50, 20, 10]
+  x = 0
+  for i in range(6):
+    q = Q[i]
+    x += int(a / q)
+    a = int(a % q)
+  if a > 0:
+    x = -1
+  return x
+print(count(150))
+print(count(750))
