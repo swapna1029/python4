c55fb53e5dd2d39968755c76993e4c76f1f5f545
swapna1029 <53587556+swapna1029@users.noreply.github.com>
2019-08-11 12:44:13 +0530

    Update simple_runtime_error.py

--- simple_runtime_error.py
+++ simple_runtime_error.py
@@ -1,6 +1,6 @@
 a = [1, 2, 3] 
 try:  
-    print "Second element = %d" %(a[1])
-    print "Fourth element = %d" %(a[3])  
+    print("Second element = %d" %(a[1]))
+    print("Fourth element = %d" %(a[3]))  
     except IndexError: 
     print "An error occurred"
